c73891cdb3fe756d459ede474f9b5cc29b5d4e80
estefani0718 <eestefanisg@gmail.com>
2024-12-06 11:25:56 -0500

    modi

--- js/ejercicio-2.js
+++ js/ejercicio-2.js
@@ -2,20 +2,20 @@
 // pantalla si la contraseña introducida por el usuario coincide con la guardada en la
 // variable sin tener en cuenta mayúsculas y minúsculas, se debe validar que solo se pueda ingresar valores alfanuméricos.
 
-let personCon=prompt(("ingrese su contraseña \n maximo 8 caracteres \n nota:ingresa valores alfanumericos "));
-let contraseña=personCon;
-const valida=function(personCon){
- let contra=/^([a-z]]{0,8})+([0-9]{2,4})$/;
- let rta=0;
- if(personCon===contra){
-    rta++;
- }
- else return false
- if(rta!==contraseña)return "la contraseña esta mal ";
- else{
-    return "vuelva a repetir la accion";
-}
 
-}
-let resultado=valida(personCon);
-console.log(resultado);
+
+   
+   let rta=0;
+   
+   while(rta==0){
+      
+      let personCon=prompt(("ingrese su contraseña \n maximo 8 caracteres \n nota:ingresa valores alfanumericos "));
+      if(/^([a-z]]{0,8})+([0-9]{2,4})$/.test(personCon))
+      {
+         alert("contraseña validada");
+         rta+=1;
+      }
+      else alert("invalidada ,volver hacerla");
+   }
+
+

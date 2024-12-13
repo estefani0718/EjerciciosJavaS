a583fc8270a8c3f994372b07f141ca834af21ec2
estefani0718 <eestefanisg@gmail.com>
2024-12-13 15:52:08 -0500

    primera parte

--- js/ejercicio-10.js
+++ js/ejercicio-10.js
@@ -5,8 +5,6 @@
 // Escribir un programa que pregunte al usuario si quiere una pizza vegetariana o no, y en
 // función de su respuesta le muestre un menú con los ingredientes disponibles para que
 // elija. Solo se puede elegir un ingrediente además de la mozzarella y el tomate que están
-
-// GFPI-F-135 V02
 // en todas las pizzas. Al final se debe mostrar por pantalla si la pizza elegida es vegetariana
 // o no y todos los ingredientes que lleva
 
